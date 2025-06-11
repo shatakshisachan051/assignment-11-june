3726381040f83e583a47c6dc62dc9d37479df17e
shatakshisachan051 <shatakshisachan1017@gmail.com>
2025-06-11 15:47:30 +0530

    7

--- middlewares/role.middleware.js
+++ middlewares/role.middleware.js
@@ -2,7 +2,7 @@ const authorize =(allowedRoles=[])=>{
     return (req,res,next)=>{
 
         const userRole= req.user?.role;
-        if(!userRole ){
+        if(!userRole || !allowedRoles.includes(userRole) ){
             return res.status(404).json({msg:"access denied "})
         }
     
